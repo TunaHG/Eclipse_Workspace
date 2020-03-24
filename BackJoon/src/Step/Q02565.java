a63d2ef065a1699d3fdb9cefcbb762860daf4d79
TunaHG <asdf0185@naver.com>
2020-03-24 17:41:12 +0900

    Update Backjoon Algorithm about LIC, LCS

--- BackJoon/src/Step/Q02565.java
+++ BackJoon/src/Step/Q02565.java
@@ -1,14 +1,34 @@
 package Step;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Q02565 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		int N = scan.nextInt();
+		int N = Integer.parseInt(br.readLine());
+		int elec[] = new int[501];
+		int dp[] = new int[501];
 		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			elec[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(int i = 1; i <= 500; i++) {
+			if(elec[i] > 0) dp[i] = 1;
+			else continue;
+			for(int j = 1; j < i; j++) {
+				if(elec[i] > elec[j] && dp[i] <= dp[j]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+			max = Math.max(max, dp[i]);
+		}
 		
 	}
 
